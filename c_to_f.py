0aa27bed48989f3f37d315f4d5ae800a6374f283
Yutung Huang <milkytrace@hotmail.com>
2018-09-24 21:28:01 +0800

    add quit function and description

--- c_to_f.py
+++ c_to_f.py
@@ -1,4 +1,11 @@
-celcius = input('請輸入攝氏溫度： ')
-celcius = float(celcius)
-F = celcius * 9 / 5 + 32
-print('華氏溫度是：',F)
+print('這是一個可以把攝氏溫度轉換為華氏的程式\n')
+print('如需退出，請輸入 q')
+while True:
+	celcius = input('請輸入攝氏溫度： ')
+	if celcius == 'q':
+		break
+	else:
+		celcius = float(celcius)
+		F = celcius * 9 / 5 + 32
+		print('華氏溫度是：',F)
+print('謝謝你使用這個程式')
